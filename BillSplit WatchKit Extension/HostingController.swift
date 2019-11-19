3e1ba96e096f4108cb4bbb8b1645865898925d63
Mark Booth <mark@markandpol.com>
2019-11-19 14:11:11 +0000

    Initial Commit

---
+++ BillSplit WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  BillSplit WatchKit Extension
+//
+//  Created by Mark Booth on 19/11/2019.
+//  Copyright © 2019 Mark Booth. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
